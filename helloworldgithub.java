27a204aa345f25898ce515303b34bfd24f6c9d6e
flaybooste <retardado.tw@gmail.com>
2015-05-29 10:50:34 -0300

    HelloWorldJOptionPane

---
+++ helloworldgithub.java
@@ -0,0 +1,9 @@
+package flay;
+import javax.swing.JOptionPane;
+public class FlayGitHub {
+
+public static void main (String [] args){
+JOptionPane.showMessageDialog (null, "Hello World !!!!");
+return ;
+}
+}
